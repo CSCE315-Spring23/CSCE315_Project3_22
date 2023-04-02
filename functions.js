6d2625232f66714fe49ebb6f2566b5f04d20f885
NairSiddharth-Boogaloo <nair.sid01@tamu.edu>
2023-04-02 12:22:26 -0500

    Convert to EJS format
    converted js file to ejs format, created necessary ejs file

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 import { Client } from 'pg';
+const express = require('express');
 
 const client = new Client({
     user: "csce315331_team_22_master",
@@ -8,22 +9,37 @@ const client = new Client({
 })
 const outputDiv = document.getElementById('output');
 
-client.connect(function (err) {
-    if (err) throw err;
+const app = express();
 
-    var query = "SELECT * FROM menu;";
-    let html = '<table><tr><th>ID</th><th>Name</th></tr>';
-    client.query(query, function(err, results) {
-        if (err) throw err;
-        for(let i = 0; i < results.length; i++) {
-            html += '<tr><td>' + results[i].menu_item_id + '</td><td>' + results[i].category + '</td><td>' + results[i].price + '</td></tr>';
-        }
-        html += '</table>';
+app.get('/menu', (req, res) => {
+  client.query('SELECT * FROM menu', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.render('menu', { menu: results });
+  });
+});
+
+
+// client.connect(function (err) {
+//     if (err) throw err;
+
+//     var query = "SELECT * FROM menu;";
+//     let html = '<table><tr><th>ID</th><th>Name</th></tr>';
+//     client.query(query, function(err, results) {
+//         if (err) throw err;
+//         for(let i = 0; i < results.length; i++) {
+//             html += '<tr><td>' + results[i].menu_item_id + '</td><td>' + results[i].category + '</td><td>' + results[i].price + '</td></tr>';
+//             console.log(results[i].menu_item_id);
+//             console.log(results[i].category);
+//             console.log(results[i].price);
+//         }
+//         html += '</table>';
         
         
-        //console.log(results) //display menu
-        outputDiv.innerHTML = html;
+//         //console.log(results) //display menu
+//         outputDiv.innerHTML = html;
 
     
-    })
-    })
+//     })
+//     })
