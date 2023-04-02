55d98f4df0e7d0fdcfc9b971a0aabcb98bc80828
NairSiddharth-Boogaloo <nair.sid01@tamu.edu>
2023-04-01 20:05:33 -0500

    tweaked code
    declared var, attempted to bugfix js not working in html file

--- functions.js
+++ functions.js
@@ -1,25 +1,29 @@
-const {Client} = require('pg')
+import { Client } from 'pg';
+
 const client = new Client({
     user: "csce315331_team_22_master",
     password: "0000",
     host: "csce-315-db.engr.tamu.edu",
     database: "csce315331_team_22"
 })
+const outputDiv = document.getElementById('output');
 
 client.connect(function (err) {
     if (err) throw err;
 
-    var query = "SELECT * FROM menu;"
+    var query = "SELECT * FROM menu;";
+    let html = '<table><tr><th>ID</th><th>Name</th></tr>';
     client.query(query, function(err, results) {
         if (err) throw err;
         for(let i = 0; i < results.length; i++) {
             html += '<tr><td>' + results[i].menu_item_id + '</td><td>' + results[i].category + '</td><td>' + results[i].price + '</td></tr>';
         }
         html += '</table>';
-        const outputDiv = document.getElementById('output');
-        outputDiv.innerHTML = html;
+        
+        
         //console.log(results) //display menu
 
         
     })
+    outputDiv.innerHTML = html;
 })
