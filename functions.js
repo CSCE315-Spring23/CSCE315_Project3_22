0da27f4c788e186208c2df4854cdd340a0ceb0b3
brianminhvu1 <brianminhvu@tamu.edu>
2023-04-03 02:48:38 -0500

    Progress on Query, Still need to display items

--- functions.js
+++ functions.js
@@ -16,9 +16,41 @@ client.connect(function (err) {
         console.log(results.rows[0].order_id) //display last orderid
         let new_order_id = Number(results.rows[0].order_id) + 1
         new_order_id = new_order_id.toString()
-        console.log(new_order_id) //display new orderid
-
+        console.log("new order id " + new_order_id) //display new orderid
         
+
+        for (let i = 0; i < 3; i++) {  //change to fit the order from customer
+                                       //ITEM_ID DOES NOT UPDATE EACH LOOP FIND FIX
+            
+            query = "SELECT * FROM orders_by_item ORDER BY item_id DESC LIMIT 1;"
+            client.query(query, function(err, results) {
+                if (err) throw err;
+
+                console.log("current item_id" + results.rows[0].item_id)
+                let new_item_id = Number(results.rows[0].item_id) + 1
+                new_item_id = new_item_id.toString()
+                console.log("new item id" + new_item_id)
+
+                var currDate = new Date()
+                var timestamp = currDate.getFullYear() + "-" + (currDate.getMonth() + 1) + "-" + currDate.getDate() + " " + currDate.getHours() + ":"  
+                + currDate.getMinutes() + ":" 
+                + currDate.getSeconds() + "." + currDate.getMilliseconds();
+                console.log(timestamp)
+
+                var item = "banana boat - 20"
+                var price = "5.69"
+                query = "INSERT INTO orders_by_item (item_id, order_id, menu_item_id, item_date, item_price) VALUES ($1, $2, $3, $4, $5)"
+                client.query(query, [new_item_id, new_order_id, item, timestamp, price], function(err, results) {
+                    if (err) throw err;
+
+                    console.log("item inserted")
+                    client.end();
+                })
+            })
+            client.end()
+
+            
+        }
     })
 })
 
