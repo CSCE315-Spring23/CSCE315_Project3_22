d5e2c53d04755156aed851d23da2b9083c4881ec
Siddesh Padala <sid.padala@tamu.edu>
2023-04-04 15:22:01 -0500

    added the menu implementation to server branch

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-const {Client} = require('pg')
+/*const {Client} = require('pg')
 const client = new Client({
     user: "csce315331_team_22_master",
     password: "0000",
@@ -11,7 +11,7 @@ const client = new Client({
 /**ORDER BY ITEM UPDATE
  * TRY TO MAKE IT ITERATE THROUGH ORDER CORRECTLY
  */
-
+/*
 client.connect(function (err) {
     if (err) throw err;
 
@@ -52,11 +52,13 @@ client.connect(function (err) {
         })
     })
 })
-
+*/
 
 /**
  * ORDER SUMMARY UPDATE
  */
+
+/*
 var query = "SELECT * FROM orders_summary ORDER BY order_id DESC LIMIT 1;"
 client.query(query, function(err, results) {
     if (err) throw err;
@@ -85,3 +87,37 @@ client.query(query, function(err, results) {
     })
 })
 
+*/
+
+
+const express = require('express');
+const { Client } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const client = new Client({
+    user: "csce315331_team_22_master",
+    password: "0000",
+    host: "csce-315-db.engr.tamu.edu",
+    database: "csce315331_team_22"
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    client.connect(function(err) {
+        if (err) throw err;
+        
+        const query = "SELECT * FROM menu;";
+        client.query(query, function(err, results) {
+            if (err) throw err;
+
+            res.render('index', { data: results.rows });
+        });
+    });
+});
+
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}`);
+});
