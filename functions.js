cb31fb2b7940ea798d13673354858c1750539727
Siddesh Padala <sid.padala@tamu.edu>
2023-04-04 01:44:57 -0500

    set up connection with database to display the menu items

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+const { Client } = require('pg');
 
 const client = new Client({
     user: "csce315331_team_22_master",
@@ -6,24 +6,22 @@ const client = new Client({
     host: "csce-315-db.engr.tamu.edu",
     database: "csce315331_team_22"
 })
-const outputDiv = document.getElementById('output');
+
+//const outputDiv = document.getElementById('output');
 
 client.connect(function (err) {
     if (err) throw err;
 
     var query = "SELECT * FROM menu;";
-    let html = '<table><tr><th>ID</th><th>Name</th></tr>';
+    let html = '<table><tr><th>ID</th><th>Category</th><th>Price</th></tr>';
     client.query(query, function(err, results) {
         if (err) throw err;
-        for(let i = 0; i < results.length; i++) {
-            html += '<tr><td>' + results[i].menu_item_id + '</td><td>' + results[i].category + '</td><td>' + results[i].price + '</td></tr>';
+        for(let i = 0; i < results.rows.length; i++) {
+            html += '<tr><td>' + results.rows[i].menu_item_id + '</td><td>' + results.rows[i].category + '</td><td>' + results.rows[i].price + '</td></tr>';
         }
         html += '</table>';
         
-        
-        //console.log(results) //display menu
-
+        console.log(html);
         
     })
-    outputDiv.innerHTML = html;
 })
