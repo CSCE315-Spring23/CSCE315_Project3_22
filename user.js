e27f455b9e3019f7e39e7797d511762f60bae21c
adam-pinto <adampinto@tamu.edu>
2023-05-02 08:02:13 -0500

    added comments and cleaned up code

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // import statements
 const express = require('express');
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 const dotenv = require('dotenv').config();
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -31,19 +31,36 @@ const pool = new Pool ({
     ssl: {rejectUnauthorized: false}
 });
 
-// user home route
+
+/**
+ * User home route that renders the user view.
+ *
+ * @function
+ * @name homeRoute
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @returns {undefined}
+ */
 app.get("/", function(req, res) {
-    // render html code and css styling
     res.render('user');
 });
 
-// route to get menu items
+
+/**
+ * Route handler for retrieving menu items from the database.
+ *
+ * @function
+ * @name menuItemsRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get("/menu-items", function(req, res) {
     const category = req.query.category;
 
     let query;
     if (category == "featured") {
-        // fetch the featured items (most selling)
+        // fetch the featured items (top 10 most selling)
         query = "SELECT menu_item_id, COUNT(menu_item_id) FROM orders_by_item GROUP BY menu_item_id ORDER BY COUNT(menu_item_id) DESC LIMIT 10;";
         pool.query(query)
         .then(result => {
@@ -68,7 +85,16 @@ app.get("/menu-items", function(req, res) {
     }
 });
 
-// route to get item ingredients
+
+/**
+ * Route handler for retrieving the ingredients for a menu item from the database.
+ *
+ * @function
+ * @name itemIngredientsRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get("/item-ingredients", function(req, res) {
     const menu_item_id = req.query.menu_item_id;
 
@@ -84,7 +110,16 @@ app.get("/item-ingredients", function(req, res) {
     });
 });
 
-// route to get additives
+
+/**
+ * Route handler for retrieving a list of 8 additives from the database for a user to choose from.
+ *
+ * @function
+ * @name additivesRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get("/additives", function(req, res) {
     let query;
     query = "SELECT * FROM additives LIMIT 8;";
@@ -98,6 +133,16 @@ app.get("/additives", function(req, res) {
     });
 });
 
+
+/**
+ * Route handler for retrieving the price of a menu item from the database.
+ *
+ * @function
+ * @name itemPriceRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get("/item-price", function(req, res) {
     const menu_item_id = req.query.menu_item_id;
 
@@ -113,67 +158,89 @@ app.get("/item-price", function(req, res) {
     });
 });
 
-// store cart information
+
+/**
+ * Route handler for storing cart information that was generated from the home page.
+ *
+ * @function
+ * @name storeCartRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.post('/store-cart', (req, res) => {
     if (!req.session.cart) {
         req.session.cart = [];
     }
 
-    // Assuming req.body contains the cart data
     req.session.cart = req.body;
     res.sendStatus(200);
 });
 
-// render the cart page
+
+/**
+ * Route handler for rendering the cart page with the cart information stored in the session.
+ *
+ * @function
+ * @name cartPageRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.get("/cart", function(req, res) {
-    // render html code and css styling
     res.render('cart', {cart: req.session.cart});
 });
 
-// store cart information
+
+/**
+ * Route handler for updating the database upon every new order.
+ *
+ * @async
+ * @function
+ * @name placeOrderRouteHandler
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
 app.post('/place-order', async (req, res) => {
     let cart = req.body;
 
-    // orders_summary = order_id, employee_id, order_date, total_price
-    // order_id - query the database to get the latest order_id
-    // SELECT order_id FROM orders_summary ORDER BY order_id DESC limit 1;
+    // set up information for queries
     let order_id = await getLatestOrderId() + 1;
-    // employee_id - random number 1-7
     let employee_id = Math.floor(Math.random() * 7) + 1;
 
-    // order_date - get the current date
     var currDate = new Date()
     var timestamp = currDate.getFullYear() + "-" + (currDate.getMonth() + 1) + "-" + currDate.getDate() + " " + currDate.getHours() + ":"  + currDate.getMinutes() + ":" + currDate.getSeconds() + "." + currDate.getMilliseconds();
     
-    // total_price - loop through each item in the cart (located in req.body)
     let total_price = 0.00;
     cart.forEach(item => {
         total_price += parseFloat(item.price);
     });
     
+    // update the orders_summary table
     await update_order_summary(order_id, employee_id, timestamp, total_price);
 
+    // get information for updating orders_by_item as well as inventory and additives
     let item_id = await getLatestItemId() + 1;
-    // Process each item in the cart
     for (let item of cart) {
-        // Get the latest item_id
         let menu_item_id = item.name;
         let item_price = parseFloat(item.price);
 
-        // Insert the item into orders_by_item
+        // update orders_by_item table
         await update_order_items(item_id, order_id, menu_item_id, timestamp, item_price);
 
-        // Update the inventory for each ingredient in the item
+        // update the inventory for each ingredient in the item
         for (let i = 0; i < item.ingredients.length; i++) {
             const ingredient = item.ingredients[i];
             await updateInventory(ingredient, menu_item_id);
         }
 
-        // Update the inventory for each additive in the item
+        // update the inventory for each additive in the item
         item.additives.forEach(async additive => {
             await updateAdditives(additive, item_id);
         });
 
+        // update straws and cups in the database
         await update_straws_cups(menu_item_id);
 
         item_id += 1;
@@ -182,11 +249,28 @@ app.post('/place-order', async (req, res) => {
     res.sendStatus(200);
 });
 
-// start web app and listen on port 3000
+
+/**
+ * Starts the web app and listens on the specified port.
+ *
+ * @function
+ * @name startServer
+ * @param {number} port - The port number to listen on.
+ * @returns {void}
+ */
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
+
+/**
+ * Retrieves the latest order ID from the database.
+ *
+ * @async
+ * @function
+ * @name getLatestOrderId
+ * @returns {Promise<number>} The latest order ID.
+ */
 async function getLatestOrderId() {
     let order_id = -1;
 
@@ -203,6 +287,15 @@ async function getLatestOrderId() {
     return order_id;
 }
 
+
+/**
+ * Retrieves the latest item ID from the orders_by_item table in the database.
+ *
+ * @async
+ * @function
+ * @name getLatestItemId
+ * @returns {Promise<number>} The latest item ID.
+ */
 async function getLatestItemId() {
     let item_id = -1;
 
@@ -219,6 +312,19 @@ async function getLatestItemId() {
     return item_id;
 }
 
+
+/**
+ * Updates the orders_summary table in the database with the given order information.
+ *
+ * @async
+ * @function
+ * @name updateOrderSummary
+ * @param {number} order_id - The ID of the order.
+ * @param {number} employee_id - The ID of the employee who fulfilled the order.
+ * @param {string} order_date - The date and time the order was placed.
+ * @param {number} total_price - The total price of the order.
+ * @returns {void}
+ */
 async function update_order_summary(order_id, employee_id, order_date, total_price) {
     query = "INSERT INTO orders_summary (order_id, employee_id, order_date, total_price) VALUES ($1, $2, $3, $4)"
     pool.query(query, [order_id, employee_id, order_date, total_price], function(err, results) {
@@ -226,6 +332,20 @@ async function update_order_summary(order_id, employee_id, order_date, total_pri
     }) 
 }
 
+
+/**
+ * Updates the orders_by_item table in the database with the given order item information.
+ *
+ * @async
+ * @function
+ * @name updateOrderItems
+ * @param {number} item_id - The ID of the order item.
+ * @param {number} order_id - The ID of the order.
+ * @param {number} menu_item_id - The ID of the menu item.
+ * @param {string} item_date - The date and time the item was ordered.
+ * @param {number} item_price - The price of the item.
+ * @returns {void}
+ */
 async function update_order_items(item_id, order_id, menu_item_id, item_date, item_price) {
     query = "INSERT INTO orders_by_item (item_id, order_id, menu_item_id, item_date, item_price) VALUES ($1, $2, $3, $4, $5);"
     pool.query(query, [item_id, order_id, menu_item_id, item_date, item_price], function(err, results) {
@@ -233,6 +353,17 @@ async function update_order_items(item_id, order_id, menu_item_id, item_date, it
     })
 }
 
+
+/**
+ * Updates the inventory in the database for the given product and menu item.
+ *
+ * @async
+ * @function
+ * @name updateInventory
+ * @param {string} product_name - The name of the product in the inventory to update.
+ * @param {number} menu_item_id - The ID of the menu item to update the inventory for.
+ * @returns {void}
+ */
 async function updateInventory(product_name, menu_item_id) {
     let product_id = await getProductID(product_name);
     
@@ -245,6 +376,17 @@ async function updateInventory(product_name, menu_item_id) {
     await update_inv_quantity(product_name, new_quantity);
 }
 
+
+/**
+ * Updates the inventory and item_additives tables in the database for the given additive and order item.
+ *
+ * @async
+ * @function
+ * @name updateAdditives
+ * @param {string} additive - The name of the additive to update.
+ * @param {number} item_id - The ID of the order item to update the additives for.
+ * @returns {void}
+ */
 async function updateAdditives(additive, item_id) {
     let product_id = await getProductID(additive);
 
@@ -260,6 +402,16 @@ async function updateAdditives(additive, item_id) {
     });
 }
 
+
+/**
+ * Gets the ID of the product with the given name from the inventory table in the database.
+ *
+ * @async
+ * @function
+ * @name getProductID
+ * @param {string} product_name - The name of the product to get the ID for.
+ * @returns {number} The ID of the product with the given name.
+ */
 async function getProductID(product_name) {
     let product_id = -1;
 
@@ -276,6 +428,17 @@ async function getProductID(product_name) {
     return product_id;
 }
 
+
+/**
+ * Gets the quantity of the product with the given ID used in the menu item with the given ID from the menu_item_ingredients table in the database.
+ *
+ * @async
+ * @function
+ * @name get_quantity
+ * @param {number} menu_item_id - The ID of the menu item to get the quantity for.
+ * @param {number} product_id - The ID of the product to get the quantity for.
+ * @returns {number} The quantity of the product with the given ID used in the menu item with the given ID.
+ */
 async function get_quantity(menu_item_id, product_id) {
     let quantity = -1;
 
@@ -292,6 +455,16 @@ async function get_quantity(menu_item_id, product_id) {
     return quantity;
 }
 
+
+/**
+ * Gets the current quantity of the product with the given name from the inventory table in the database.
+ *
+ * @async
+ * @function
+ * @name get_inv_quantity
+ * @param {string} product_name - The name of the product to get the quantity for.
+ * @returns {number} The current quantity of the product with the given name.
+ */
 async function get_inv_quantity(product_name) {
     let quantity = -1;
 
@@ -308,6 +481,16 @@ async function get_inv_quantity(product_name) {
     return quantity;
 }
 
+
+/**
+ * Updates the current quantity of the product with the given name in the inventory table in the database to the given new quantity.
+ *
+ * @async
+ * @function
+ * @name update_inv_quantity
+ * @param {string} product_name - The name of the product to update the quantity for.
+ * @param {number} new_quantity - The new quantity to set for the product.
+ */
 async function update_inv_quantity(product_name, new_quantity) {
     query = "UPDATE inventory SET quantity= $1 WHERE product_name= $2;"
     pool.query(query, [new_quantity, product_name], function(err, results) {
@@ -315,6 +498,15 @@ async function update_inv_quantity(product_name, new_quantity) {
     })
 }
 
+
+/**
+ * Updates the inventory for cups and straws based on the size of the menu item.
+ *
+ * @async
+ * @function
+ * @name update_straws_cups
+ * @param {string} menu_item_id - The id of the menu item to update the inventory for.
+ */
 async function update_straws_cups(menu_item_id) {
     // update cups
     let size = menu_item_id.substr(-2);
