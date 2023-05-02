099efca59c23f162585625e366a969a3f4b858dc
NairSiddharth-Boogaloo <nair.sid01@tamu.edu>
2023-05-02 18:34:59 -0500

    Update xz_report.js

--- public/manager/reports/xz_report.js
+++ public/manager/reports/xz_report.js
@@ -9,21 +9,52 @@
  * @returns {void}
  */
 function xz_report() {
+//     var xhr = new XMLHttpRequest();
+//     const today = new Date();    
+//     const year = today.getFullYear(); // get the year (yyyy)
+//     const month = ('0' + (today.getMonth() + 1)).slice(-2); // get the month (mm)
+//     const day = ('0' + today.getDate()).slice(-2); // get the day (dd)
+//     const formattedDate = `${year}-${month}-${day}`;
+    
+//     xhr.onload = () => {
+//         // menu, menu_ingredients
+//         var response = JSON.parse(xhr.response);
+//         var table = '<table><thead><tr><th>Report Type</th><th>Date</th><th>Total Sales</th></tr></thead><tbody>';
+//         for (var i = 0; i < response.xz.length; i++) {
+//             table += '<tr>';
+//             response.xz.length
+//             if (formattedDate === response.xz._date.toString() && today.getHours() < 17) {
+//                 table += '<td>' + "x report" + '</td>';
+//             }
+//             else {
+//                 table += '<td>' + "z report" + '</td>';
+//             }
+//             table += '<td>' + response.xz.report_date + '</td>';
+//             table += '<td>' + response.xz.daily_sales + '</td>';
+//             table += '</tr>';
+//         }
+//         table += '</tbody></table>';
+//         $('#modal_table').html(table);
+//     }
+//     xhr.open("PUT", "/manager/reports/xz_report", true);
+//     xhr.setRequestHeader("Content-type", "application/json");
+//     xhr.send();
+// }
     var xhr = new XMLHttpRequest();
     var table = '<table><thead><tr><th>Report Type</th><th>Date</th><th>Total Sales</th></tr></thead><tbody>';
     var total_sales = 0;
 
     xhr.onload = () => { 
     var response = JSON.parse(xhr.response);
-    var prev_splitted = response.xz.order_date;
+    var prev_splitted = response.xz[0].order_date;
     for (var i = 1; i < response.xz.length; i++) {
-        var splitted = response.xz.order_date;
+        var splitted = response.xz[i].order_date;
         var row = [];
         var myobj = new Date();
         var formattedDate = myobj.getFullYear() + "-" + ('0' + (myobj.getMonth() + 1)).slice(-2) + "-" + ('0' + myobj.getDate()).slice(-2);
         
         //handles old z reports 
-        total_sales += response.xz.total_price;
+        total_sales += response.xz[i].total_price;
         if (prev_splitted !== splitted) {
             if (splitted === formattedDate && myobj.getHours() < 17) {
                 row.push("x report");
