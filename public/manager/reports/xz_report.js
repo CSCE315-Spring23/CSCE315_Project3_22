e553a2766e073076eef23822ec447ab3423b385e
NairSiddharth-Boogaloo <nair.sid01@tamu.edu>
2023-05-02 18:11:17 -0500

    Update xz_report.js

--- public/manager/reports/xz_report.js
+++ public/manager/reports/xz_report.js
@@ -15,23 +15,23 @@ function xz_report() {
 
     xhr.onload = () => { 
     var response = JSON.parse(xhr.response);
-    var prev_splitted = response.xz[0][2].split(" ");
-    for (var i = 0; i < response.xz.length-1; i++) {
-        var splitted = response.xz[i][2].split(" ");
+    var prev_splitted = response.xz.order_date;
+    for (var i = 1; i < response.xz.length; i++) {
+        var splitted = response.xz.order_date;
         var row = [];
         var myobj = new Date();
         var formattedDate = myobj.getFullYear() + "-" + ('0' + (myobj.getMonth() + 1)).slice(-2) + "-" + ('0' + myobj.getDate()).slice(-2);
         
         //handles old z reports 
-        total_sales += response.xz[i][3];
-        if (prev_splitted[0] !== splitted[0]) {
-            if (splitted[0] === formattedDate && myobj.getHours() < 17) {
+        total_sales += response.xz.total_price;
+        if (prev_splitted !== splitted) {
+            if (splitted === formattedDate && myobj.getHours() < 17) {
                 row.push("x report");
             }
             else {
                 row.push("z report");
             }
-            row.push(splitted[0]);
+            row.push(splitted);
             row.push(total_sales);
             table += '<tr><td>' + row[0] + '</td><td>' + row[1] + '</td><td>' + row[2] + '</td></tr>';
             total_sales = 0; 
